2284fd9eb22306f5ad4173dc6968a957d8b05e76
LucasFebatis <lfebatis@gmail.com>
2024-08-31 18:42:50 -0300

    feat: add initial ios project

---
+++ ios random quote/ios random quote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios random quote
+//
+//  Created by Lucas Batista on 31/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
